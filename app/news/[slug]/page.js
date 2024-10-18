9daf9ae49804b844883b492da574b60f15b2eaf4
Safa Gayret <safagayret@gmail.com>
2024-10-18 15:01:58 +0300

    fix: style

--- app/news/[slug]/page.js
+++ app/news/[slug]/page.js
@@ -1,22 +1,22 @@
-import { fetchAirtableTable } from "@/app/lib/airtable";
-import { formatDateTime } from "@/app/lib/formatDate";
-import Link from "next/link";
-import locationSvg from "../../img/location-sign.svg";
-import Image from "next/image";
+import { fetchAirtableTable } from '@/app/lib/airtable'
+import { formatDateTime } from '@/app/lib/formatDate'
+import Link from 'next/link'
+import locationSvg from '../../img/location-sign.svg'
+import Image from 'next/image'
 
-export const revalidate = 60;
+export const revalidate = 60
 
 export default async function News({ params }) {
-  const data = await fetchAirtableTable("news");
+  const data = await fetchAirtableTable('news')
 
-  const decodedSlug = decodeURIComponent(params.slug);
+  const decodedSlug = decodeURIComponent(params.slug)
 
-  const news = data.find((n) => n.slug === decodedSlug);
+  const news = data.find((n) => n.slug === decodedSlug)
 
-  const logs = await fetchAirtableTable("news_logs");
+  const logs = await fetchAirtableTable('news_logs')
 
   data.forEach((news) => {
-    news.logs = [];
+    news.logs = []
 
     logs.forEach((log) => {
       if (news.id === log.parent_new[0]) {
@@ -25,31 +25,25 @@ export default async function News({ params }) {
           date: log.date,
           description: log.title,
           created: log.created,
-        });
+        })
       }
-    });
-  });
+    })
+  })
 
   function getSource(source) {
-    const url = new URL(source);
-    if (url.hostname.includes("www")) {
-      return url.hostname.split(".")[1].toUpperCase();
+    const url = new URL(source)
+    if (url.hostname.includes('www')) {
+      return url.hostname.split('.')[1].toUpperCase()
     } else {
-      return url.hostname;
+      return url.hostname
     }
   }
 
   return (
-    <div className="news-detail">
+    <div className='news-detail'>
       <h1>{news.title}</h1>
       <p>
-        <Image
-          src={locationSvg}
-          alt="Location"
-          className="location-svg"
-          width={12}
-          height={12}
-        />
+        <Image src={locationSvg} alt='Location' className='location-svg' width={12} height={12} />
         {news.location}
       </p>
       <p>
@@ -68,29 +62,25 @@ export default async function News({ params }) {
         <strong>Özet</strong>: {news.description}
       </p>
 
-      <div className="logs">
-        <strong>Gelişmeler</strong>
+      <div className='logs'>
         {news.logs &&
           news.logs.map((log, index) => (
-            <div className="log-items" key={index}>
-              <div className="log-item" key={index}>
-                <div className="log-item-header">
+            <div className='log-items' key={index}>
+              <div className='log-item' key={index}>
+                <div className='log-item-header'>
                   <a href={log.source}>
-                    <span className="source">{getSource(log.source)}</span>
+                    <span className='source'>{getSource(log.source)}</span>
                   </a>
-                  <span className="date">{formatDateTime(log.created)}</span>
+                  <span className='date'>{formatDateTime(log.created)}</span>
                 </div>
-                <span className="description">{log.description}</span>
+                <span className='description'>{log.description}</span>
               </div>
             </div>
           ))}
-        <a
-          href="https://airtable.com/appGCdm8zSGJP5ydL/pagzYQZwNN4wWmVwJ/form"
-          className="btn"
-        >
+        <a href='https://airtable.com/appGCdm8zSGJP5ydL/pagzYQZwNN4wWmVwJ/form' className='btn'>
           + Yeni gelişme ekle
         </a>
       </div>
     </div>
-  );
+  )
 }
--- app/page.js
+++ app/page.js
@@ -1,18 +1,18 @@
-import Search from "./components/Search/search";
-import { fetchAirtableTable } from "./lib/airtable";
-import { formatDateTime } from "./lib/formatDate";
-import Link from "next/link";
-import locationSvg from "./img/location-sign.svg";
-import Image from "next/image";
+import Search from './components/Search/search'
+import { fetchAirtableTable } from './lib/airtable'
+import { formatDateTime } from './lib/formatDate'
+import Link from 'next/link'
+import locationSvg from './img/location-sign.svg'
+import Image from 'next/image'
 
-export const revalidate = 60;
+export const revalidate = 60
 
 export default async function Home() {
-  const data = await fetchAirtableTable("news");
-  const logs = await fetchAirtableTable("news_logs");
+  const data = await fetchAirtableTable('news')
+  const logs = await fetchAirtableTable('news_logs')
 
   data.forEach((news) => {
-    news.logs = [];
+    news.logs = []
 
     logs.forEach((log) => {
       if (news.id === log.parent_new[0]) {
@@ -21,76 +21,66 @@ export default async function Home() {
           date: log.date,
           description: log.title,
           created: log.created,
-        });
+        })
       }
-    });
-  });
+    })
+  })
 
   function getSource(source) {
-    const url = new URL(source);
-    if (url.hostname.includes("www")) {
-      return url.hostname.split(".")[1].toUpperCase();
+    const url = new URL(source)
+    if (url.hostname.includes('www')) {
+      return url.hostname.split('.')[1].toUpperCase()
     } else {
-      return url.hostname;
+      return url.hostname
     }
   }
 
   return (
     <>
-      <section className="news">
-        <div className="news-item static-widget">
-          <p>
-            Haber eklemek isterseniz, haberin platformda bulunup bulunmadığını
-            kontrol edin.
-          </p>
+      <section className='news'>
+        <div className='news-item static-widget'>
+          <p>Haber eklemek isterseniz, haberin platformda bulunup bulunmadığını kontrol edin.</p>
           <Search data={data} />
-          <p>
-            Eğer aradığınız haber platformda yer almıyorsa oluşturabilirsiniz.
-          </p>
+          <p>Eğer aradığınız haber platformda yer almıyorsa oluşturabilirsiniz.</p>
           <Link
-            href="https://airtable.com/appGCdm8zSGJP5ydL/pagSAXkdbeAxobZu0/form"
-            className="btn"
+            href='https://airtable.com/appGCdm8zSGJP5ydL/pagSAXkdbeAxobZu0/form'
+            className='btn'
           >
             + Yeni haber ekle
           </Link>
         </div>
         {data &&
           data.map((item, index) => (
-            <div className="news-item" key={index}>
+            <div className='news-item' key={index}>
               <Link href={`/news/${item.slug}`}>
                 <h3>{item.title}</h3>
               </Link>
-              <p className="location">
+              <p className='location'>
                 <Image
-                  className="location-svg"
+                  className='location-svg'
                   src={locationSvg}
-                  alt="location"
+                  alt='location'
                   width={12}
                   height={12}
                 />
                 {item.location}
               </p>
-              <div className="logs">
-                <strong>Gelişmeler</strong>
+              <div className='logs'>
                 {item.logs &&
                   item.logs.map((log, index) => (
-                    <div className="log-item" key={index}>
-                      <div className="log-item-header">
+                    <div className='log-item' key={index}>
+                      <div className='log-item-header'>
                         <a href={log.source}>
-                          <span className="source">
-                            {getSource(log.source)}
-                          </span>
+                          <span className='source'>{getSource(log.source)}</span>
                         </a>
-                        <span className="date">
-                          {formatDateTime(log.created)}
-                        </span>
+                        <span className='date'>{formatDateTime(log.created)}</span>
                       </div>
-                      <span className="description">{log.description}</span>
+                      <span className='description'>{log.description}</span>
                     </div>
                   ))}
                 <Link
-                  href="https://airtable.com/appGCdm8zSGJP5ydL/pagzYQZwNN4wWmVwJ/form"
-                  className="btn"
+                  href='https://airtable.com/appGCdm8zSGJP5ydL/pagzYQZwNN4wWmVwJ/form'
+                  className='btn'
                 >
                   + Yeni gelişme ekle
                 </Link>
@@ -99,5 +89,5 @@ export default async function Home() {
           ))}
       </section>
     </>
-  );
+  )
 }
