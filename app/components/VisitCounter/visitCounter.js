cbcffb667ac30e81798fa7e363dadd4831777356
Safa Gayret <safagayret@gmail.com>
2024-10-29 13:35:20 +0300

    fix: visit counter

--- app/components/VisitCounter/visitCounter.js
+++ app/components/VisitCounter/visitCounter.js
@@ -6,7 +6,7 @@ export default function VisitCounter() {
   const pathname = usePathname()
 
   function imageSourceGenerator() {
-    return `https://visit-counter.vercel.app/counter.png?page=kamuvicdani.org${pathname}&bg=222222`
+    return `https://visit-counter.vercel.app/counter.png?page=kamuvicdani.org${pathname}&bg=222222&${new Date().getTime()}`
   }
 
   return (
@@ -15,8 +15,8 @@ export default function VisitCounter() {
         className='visit-counter-img'
         src={imageSourceGenerator()}
         alt='visit-counter'
-        width={50}
-        height={50}
+        width={40}
+        height={40}
       />
       <span>Sayfanın şimdiye kadar görüntülenme sayısı</span>
     </div>
